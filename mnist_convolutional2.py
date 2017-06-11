ca66f9c45dfb02b96df85dd52685649d1783b0fd
Oleg Kalyta <2xyligan@gmail.com>
2017-06-11 14:09:34 +0300

    add scalars and images to summary

--- mnist_convolutional2.py
+++ mnist_convolutional2.py
@@ -13,6 +13,10 @@ def conv_layer(input, channels_in, channels_out, name="conv"):
         b = tf.Variable(tf.zeros([channels_out]), name="B")
         conv = tf.nn.conv2d(input, w, strides=[1, 1, 1, 1], padding="SAME")
         act = tf.nn.relu(conv + b)
+        tf.summary.histogram("weights", w)
+        tf.summary.histogram("biases", b)
+        tf.summary.histogram("activations", act)
+
         return act
 
 
@@ -40,6 +44,7 @@ logits = fc_layer(fc1, 1024, 10, "fc2")
 
 with tf.name_scope("cross_entropy"):
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+    tf.summary.scalar("cross_entropy", cross_entropy)
 
 with tf.name_scope("train"):
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
@@ -47,8 +52,12 @@ with tf.name_scope("train"):
 with tf.name_scope("accuracy"):
     correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.summary.scalar("accuracy", accuracy)
 
-writer = tf.summary.FileWriter('logs/mnist_demo/2')
+tf.summary.image("input", x_image, 3)
+
+merged_summary = tf.summary.merge_all()
+writer = tf.summary.FileWriter('logs/mnist_demo/3')
 writer.add_graph(sess.graph)
 
 sess.run(tf.global_variables_initializer())
@@ -57,5 +66,9 @@ for i in range(1000):
     if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={x: batch[0], y: batch[1]})
         print("step %d, training accuracy %g" % (i, train_accuracy))
+
+    if i % 5 == 0:
+        s = sess.run(merged_summary, feed_dict={x: batch[0], y: batch[1]})
+        writer.add_summary(s, i)
 
     train_step.run(feed_dict={x: batch[0], y: batch[1]})
